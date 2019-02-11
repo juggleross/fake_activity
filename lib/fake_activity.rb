83020c4edb7d6a4d47f54eeeaf4b0622acd53c36
Vladislav Hilko <vladislav.spawn@gmail.com>
2019-02-11 16:53:04 +0300

    test

--- lib/fake_activity.rb
+++ lib/fake_activity.rb
@@ -1,21 +1,52 @@
+require 'date'
 require "fake_activity/version"
 
 module FakeActivity
   class ActivityGenerator
-    attr_reader :from, :to
+    attr_reader :from, :to, :temp_file_name
 
     def self.generate_activity(from:, to:)
       new(from: from, to: to).generate_activity
     end
 
     def initialize(from:, to:)
-      @from = from
-      @to   = to
+      @from               = Date.parse(from)
+      @to                 = Date.parse(to)
+      @temp_file_name     = 'temp.txt'
     end
 
     def generate_activity
-      p 'is it working?'
-      `touch 1.txt`
+      iterate_by_days
+    end
+
+    private
+
+    def iterate_by_days
+      (from..to).each do |date_point|
+        add_dark_green_activity(date_point.to_s)
+      end
+    end
+
+
+    def add_light_green_activity(date_point)
+      1.times { add_commit(date_point) }
+    end
+
+    def add_moderate_green_activity(date_point)
+      6.times { add_commit(date_point) }
+    end
+
+    def add_green_activity(date_point)
+      8.times { add_commit(date_point) }
+    end
+
+    def add_dark_green_activity(date_point)
+      10.times { add_commit(date_point) }
+    end
+
+
+    def add_commit(date_point)
+      `git commit -m "temp" --allow-empty --date=#{date_point}`
     end
   end
 end
